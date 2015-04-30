e6cb6e6773ca5f5563376704b699ea50c9b47c62
twluo <twluo@ucsd.edu>
2015-04-29 23:13:18 -0700

    Revert "S"
    This reverts commit f39f468956ebe7fc14a3f0c686c446eb41c32ab9.

--- Miro/main.cpp
+++ Miro/main.cpp
@@ -88,7 +88,7 @@ makeBunnyScene()
 
 	Material* bmat = new Lambert(Vector3(1.0f), Vector3(0.0f), Vector3(1.0f),
 								0.4, 0, 0.2,
-								0, 0.3,0.7, 1.4);
+								1, 0.3, 0.7, 1.4);
 
     TriangleMesh * bunny = new TriangleMesh;
 
@@ -107,9 +107,9 @@ makeBunnyScene()
     // create the floor triangle
     TriangleMesh * floor = new TriangleMesh;
     floor->createSingleTriangle();
-    floor->setV1(Vector3(  0, 0,  10));
-    floor->setV2(Vector3( 10, 0, -10));
-    floor->setV3(Vector3(-10, 0, -10));
+    floor->setV1(Vector3(  0, -1,  10));
+    floor->setV2(Vector3( 10, -1, -10));
+    floor->setV3(Vector3(-10, -1, -10));
     floor->setN1(Vector3(0, 1, 0));
     floor->setN2(Vector3(0, 1, 0));
     floor->setN3(Vector3(0, 1, 0));
